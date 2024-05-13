b61a99539d55c5c740d16ec9f85b357772bb5121
Saif Hasan <saifhassan399@gmail.com>
2024-05-13 19:13:53 +0600

    added single post load by id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 500;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USRPASS}@cluster0.mal3t53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -35,6 +35,22 @@ async function run() {
     app.post('/addqueries', async (req, res) => {
       const query = req.body;
       const result = await queriesCollction.insertOne(query);
+      res.send(result);
+    });
+
+    // User Query By Email
+    app.get('/myqueries', async (req, res) => {
+      const email = req.query.email;
+      const query = { user_email: email };
+      const result = await queriesCollction.find(query).toArray();
+      res.send(result);
+    });
+
+    //User Single Query by ID
+    app.get('/myqueries/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await queriesCollction.findOne(query);
       res.send(result);
     });
 
