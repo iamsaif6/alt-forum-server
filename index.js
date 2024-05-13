43f0cc239d073f050f72a280c686ef30467332ca
Saif Hasan <saifhassan399@gmail.com>
2024-05-13 13:00:23 +0600

    User query api added

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5500;
+require('dotenv').config();
+const port = process.env.PORT || 500;
 
 // MiddleWare
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USRPASS}@cluster0.mal3t53.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const queriesCollction = client.db('AltForum').collection('queries');
+
+    app.get('/', (req, res) => {
+      res.send('Server is Running');
+    });
+
+    //Add User Query
+    app.post('/addqueries', async (req, res) => {
+      const query = req.body;
+      const result = await queriesCollction.insertOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Server is Running');
