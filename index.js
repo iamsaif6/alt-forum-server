7e51cc3ef6957ee3ebe96a62c3693e04efd139b3
Saif Hasan <saifhassan399@gmail.com>
2024-05-15 23:43:13 +0600

    origin updated

--- index.js
+++ index.js
@@ -60,9 +60,8 @@ async function run() {
       res
         .cookie('token', token, {
           httpOnly: true,
-          secure: false,
-          //   sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          sameSite: 'none',
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
         })
         .send({ success: true });
     });
